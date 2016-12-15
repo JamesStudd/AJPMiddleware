5b13eede3e97db1c97827109e611f91ae1a2ed4b
Chris <iamchris88@live.co.uk>
2016-12-15 17:48:34 +0000

    basics of portal added

--- Middleware/src/Agents/Portal.java
+++ Middleware/src/Agents/Portal.java
@@ -5,15 +5,74 @@
  */
 package Agents;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.Message;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.LinkedBlockingQueue;
+import javax.management.monitor.Monitor;
+
 /**
  *
  * @author chris
  */
-public class Portal {
+public class Portal implements Runnable{
+
+	
+	
+	private Set<UserAgent> children = new HashSet<UserAgent>();
+	private UserAgent parent = null;
+	public  LinkedBlockingQueue<Message> queue = new LinkedBlockingQueue<>();
+	private Thread thread = new Thread();
+	private Set<NodeMonitor> monitors = new HashSet<NodeMonitor>();
+
 
 	public Portal() {
-		System.out.println("I am a test");
+		thread.start();
+	}
+
+	private void addChild(UserAgent child){
+		children.add(child);
+	}
+
+	private void removeChild(UserAgent child){
+		children.remove(child);
+	}
+
+	private void addMonitor(Monitor mon){
+		monitors.add(mon);
+	}
+
+	private void removeMontior(Monitor mon){
+		monitors.remove(mon);
 	}
 	
+	public void addToQueue(Message message){
+		queue.add(message);
+	}
+
+
+	private void updateMonitors(Message message){
+		Iterator<NodeMonitor> it = monitors.iterator();
+		while(it.hasNext()){
+			it.next().addToQueue(Message message);
+		}
+	}
+
+	private void handle(Message message){
+		updateMonitors(message);
+
+		
+	}
+
+	@Override
+	public void run() {
+
+		while(true){
+			handle(queue.remove());
+			
+		}
+	}
+
 
 }
