48a11cd1b55f9effd1bcf048cab313e5419bc609
Chris <iamchris88@live.co.uk>
2016-12-20 13:48:16 +0000

    Created a filtered address book for scoping

--- MiddlewareProject/src/Agents/Portal.java
+++ MiddlewareProject/src/Agents/Portal.java
@@ -10,16 +10,17 @@ import Messages.MessageType;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author chris
  */
 public class Portal extends MetaAgent {
-	
-	
+
 	//The children of the node
 	private Set<MetaAgent> children = new HashSet<MetaAgent>();
 
@@ -28,9 +29,9 @@ public class Portal extends MetaAgent {
 
 	//Should be maybe moved to the meta agent, this of course depending on what we do with the nodeMonitor
 	private HashMap<MetaAgent, MetaAgent> registeredAddresses = new HashMap<MetaAgent, MetaAgent>();
-        
-        //Undelivered messages
-        private HashMap<MetaAgent, Message> lostMessages = new HashMap<MetaAgent, Message>();
+
+	//Undelivered messages
+	private HashMap<MetaAgent, Message> lostMessages = new HashMap<MetaAgent, Message>();
 
 	//This should be in the meta object
 	private LinkedBlockingQueue<Message> queue = new LinkedBlockingQueue<>();
@@ -41,7 +42,6 @@ public class Portal extends MetaAgent {
 	//A set of all the active monitors of the portal
 	private Set<NodeMonitor> monitors = new HashSet<NodeMonitor>();
 
-
 	//Constructor starts the thread
 	public Portal(String name) {
 		super(name);
@@ -49,101 +49,108 @@ public class Portal extends MetaAgent {
 	}
 
 	//Adds a node to its children
-	private void addChild(MetaAgent child){
+	private void addChild(MetaAgent child) {
 		children.add(child);
 	}
-        
-        //Removes a node from its children
-        private void removeChild(MetaAgent child){
-            children.remove(child);
-        }
+
+	//Removes a node from its children
+	private void removeChild(MetaAgent child) {
+		children.remove(child);
+	}
 
 	//Adds a monitor to report to with messages
-	private void addMonitor(MetaAgent mon){
+	private void addMonitor(MetaAgent mon) {
 		monitors.add(mon);
 	}
 
 	//Removes a monitor 
-	private void removeMontior(MetaAgent mon){
+	private void removeMontior(MetaAgent mon) {
 		monitors.remove(mon);
 	}
-	
+
 	//Public enabling others to add messages to its queue
-	public void addToQueue(Message message){
+	public void addToQueue(Message message) {
 		queue.add(message);
 	}
 
 	//Forwards a message on to the recepients 
-	private void sendMessage(Message message){
+	private void sendMessage(Message message) {
 		MetaAgent recepient = message.getRecepient();
-		if(registeredAddresses.containsKey(recepient)){
+		if (registeredAddresses.containsKey(recepient)) {
 			registeredAddresses.get(recepient).addToQueue(message);
-		}
-		else{
-			
+		} else {
+
 		}
 	}
-
 
 	//Updates all current monitors with information about messages coming through
-	private void updateMonitors(Message message){
+	private void updateMonitors(Message message) {
 		Iterator<NodeMonitor> it = monitors.iterator();
-		while(it.hasNext()){
-			it.next().addToQueue(Message message);
+		while (it.hasNext()) {
+			it.next().addToQueue(Message message
+		
+	
+
+	);
 		}
 
 	}
 
-	private boolean isForMe(MetaAgent x){
+	private boolean isForMe(MetaAgent x) {
 		return x == this;
 	}
 
 	//Runs through all the children and updates with current addressbook
-	private void updateChildrenWithAddressBook(MetaAgent scope){
+	private void updateChildrenWithAddressBook(MetaAgent scope) {
 
-		for (MetaAgent x : children){
+		for (MetaAgent x : children) {
 			x.addToQueue(new Message(MessageType.UPDATE_ADDRESSES, this, x, registeredAddresses, scope));
 		}
 	}
 
 	//Updates parent with address book
-	private void updateParentWithAddressBook(MetaAgent scope){
-		parent.addToQueue(new Message(MessageType.UPDATE_ADDRESSES, this, parent, registeredAddresses, scope));
+	private void updateParentWithAddressBook() {
+		Map<MetaAgent, MetaAgent> filtered = registeredAddresses.keySet().stream()
+			.filter(map -> map.getScope() == this)
+			.collect(Collectors.toMap(p -> p, p -> registeredAddresses.get(p)));
+
+		parent.addToQueue(new Message(MessageType.UPDATE_ADDRESSES, this, parent, filtered));
 	}
 
 	//Adds a node  to the portals children and updates the address
-	private void addNode(MetaAgent node){
+	private void addNode(MetaAgent node) {
 
 		children.add(node);
 		registeredAddresses.put(node, this);
 		updateChildrenWithAddressBook(node.getScope());
-		if(!scopedHere(node.getScope()))
-			updateParentWithAddressBook(node.getScope());
+		if (!scopedHere(node.getScope())) {
+			updateParentWithAddressBook();
+		}
 	}
 
-	private boolean scopedHere(MetaAgent scope){
+	private boolean scopedHere(MetaAgent scope) {
 		return scope == this;
 	}
 
-	private void updateAddressBook(Message message){
-		HashMap<MetaAgent, MetaAgent> passedIn = (HashMap < MetaAgent, MetaAgent
-								>) message.retrieveMessageItem();
-		if(passedIn.equals(registeredAddresses)){
+	private void updateAddressBook(Message message) {
+		HashMap<MetaAgent, MetaAgent> passedIn = (HashMap< MetaAgent, MetaAgent>) message.retrieveMessageItem();
+		if (passedIn.equals(registeredAddresses)) {
 			return;
 		}
-		
+
 		registeredAddresses.putAll(passedIn);
-		updateChildrenWithAddressBook(message.getScope());
-		if(!scopedHere(message.getScope())){
-			updateParentWithAddressBook(message.getScope());
+		updateChildrenWithAddressBook();
+		updateParentWithAddressBook();
+		if (!scopedHere(message.getScope())) {
+			updateParentWithAddressBook();
 		}
-		
+
 	}
 
 	//Extracts the message details and handles as appropriate
-	private void extractMessageDetailsAndHandle(Message message){
-		
-		switch(message.getMessageType()){
+	private void extractMessageDetailsAndHandle(Message message) {
+
+		switch (message.getMessageType()) {
 
 			case PASS_MESSAGE:
 				String theMessage = (String) message.retrieveMessageItem();
@@ -154,20 +161,17 @@ public class Portal extends MetaAgent {
 			case UPDATE_ADDRESSES:
 				updateAddressBook(message);
 
-
-			
 		}
 
 	}
 
 	//Looks up the message address and passes on the message if its held in registered addresses
 	//otherwise an error is send back and the message added to lost messages
-	private void lookUpAndPassOn(Message message){
+	private void lookUpAndPassOn(Message message) {
 
-		if(registeredAddresses.containsKey(message.getRecipient())){
+		if (registeredAddresses.containsKey(message.getRecipient())) {
 			registeredAddresses.get(message.getRecipient()).addToQueue(message);
-		}
-		else{
+		} else {
 			//need to amend to make bounded and also think about what happens if it is already contained in the map
 			lostMessages.put(message.getRecipient(), message);
 			registeredAddresses.get(message.getSender()).addToQueue(new Message(MessageType.ADDRESS_NOT_FOUND_IN_LOST_PROPERTY, this, message.getSender(), null));
@@ -176,29 +180,27 @@ public class Portal extends MetaAgent {
 	}
 
 	//Handles a message pull
-	private void handle(Message message){
-		updateMonitors(message);		
-		if(isForMe(message.getRecipient())){
+	private void handle(Message message) {
+		updateMonitors(message);
+		if (isForMe(message.getRecipient())) {
 			extractMessageDetailsAndHandle(message);
-		}
-		else{
+		} else {
 			lookUpAndPassOn(message);
 		}
 	}
-        
-        //Merge 2 portals together
-        private void mergePortal(MetaAgent portal){
-            //something about merging portals here
-        }
+
+	//Merge 2 portals together
+	private void mergePortal(MetaAgent portal) {
+		//something about merging portals here
+	}
 
 	@Override
 	public void run() {
 
-		while(true){
+		while (true) {
 			handle(queue.remove());
-			
+
 		}
 	}
-
 
 }
