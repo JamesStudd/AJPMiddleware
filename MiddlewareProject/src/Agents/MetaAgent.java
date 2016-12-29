f8281aea6d83c10943940480d3185e499946f577
Chris <iamchris88@live.co.uk>
2016-12-29 15:02:37 +0000

    Implemented thread correctly

--- MiddlewareProject/src/Agents/MetaAgent.java
+++ MiddlewareProject/src/Agents/MetaAgent.java
@@ -13,10 +13,13 @@ import java.util.concurrent.LinkedBlockingQueue;
  * @author Sean
  */public abstract class MetaAgent implements Runnable {
      
+	 private Thread objectThread; 
         //constructor for metaagents only takes in name atm, needs to have other things overloaded onto it from subclasses like scope, parent
         public MetaAgent(String name) {   
-            thread.start();
             this.name = name;
+	    objectThread = new Thread(this);
+	    objectThread.start();
+
         }
         
         //Name for user ease/debugging
@@ -32,13 +35,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 	
 	//Public enabling others to add messages to its queue
 	public void addToQueue(Message message){
+		System.out.println("\n\n\n");
+		System.out.println("adding to queue");
+		System.out.println(message.getMessageType());
+		System.out.println("from = " + message.getSender());
+		System.out.println("to = " + message.getRecipient());
 		queue.add(message);
 	}
 
 	//Handles a message pull
-	private void handle(Message message){
-		updateMonitors(message);		
-	}
+	abstract void handle(Message message);
 
 
 	public MetaAgent getScope(){
@@ -47,9 +53,16 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 
 	@Override
+	public String toString() {
+		return name;
+	}
+
+
+	@Override
 	public void run() {
 
 		while(true){
+			if(!queue.isEmpty())
 			handle(queue.remove());
 			
 		}
